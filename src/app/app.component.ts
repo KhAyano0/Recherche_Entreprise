318ca88603c7bbf8f53f05182772fb22480af37b
Christophe <dossantos.christophe16@gmail.com>
2024-06-05 14:39:58 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormControl,FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Recherche d`Entreprise';
 }
